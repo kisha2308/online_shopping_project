11f5083e065c39b14609c682e03b8519fc655927
kisha2308 <134995241+kisha2308@users.noreply.github.com>
2024-02-07 11:21:00 +0530

    Update ConnectionProvider.java

--- Online Shopping Project/src/main/java/project/ConnectionProvider.java
+++ Online Shopping Project/src/main/java/project/ConnectionProvider.java
@@ -1,21 +1,10 @@
 package project;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Properties;
 
 public class ConnectionProvider {
-//	public static void main(String []args) throws Exception {
-//		
-//		FileReader reader = new FileReader("dbconfig.properties");
-//		Properties properties = new Properties();
-//		properties.load(reader);
-//		
-//		
-//	}
-	
 	public static Connection getCon(){
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
